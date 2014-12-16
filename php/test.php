d35e4e213b5142eb6e6df89123a2f79d0e42241d
Jeremy Ko <jeremyko193@gmail.com>
2014-12-16 00:12:36 -0800

    Change tabs to spaces

--- php/test.php
+++ php/test.php
@@ -1,36 +1,36 @@
 <?php 
-	$name = $_POST["name"];
-	$name = ($name ? $name : 'No name');
-	$message = $_POST["message"];
-	$email = $_POST["email"];
+    $name = $_POST["name"];
+    $name = ($name ? $name : 'No name');
+    $message = $_POST["message"];
+    $email = $_POST["email"];
 
-	require "PHPMailerAutoload.php";
-	$mail = new PHPMailer();
+    require "PHPMailerAutoload.php";
+    $mail = new PHPMailer();
 
-	$mail->SMTPDebug = 3;
+    $mail->SMTPDebug = 3;
 
-	$mail->isSMTP();                                      // Set mailer to use SMTP
-	$mail->Host = "smtp.gmail.com";  // Specify main and backup SMTP servers
-	$mail->SMTPAuth = true;                               // Enable SMTP authentication
-	$mail->Username = "hash.coding.edge@gmail.com";                 // SMTP username
-	$mail->Password = "#trucoder";                           // SMTP password
-	$mail->SMTPSecure = "ssl";                         // Enable TLS encryption, `ssl` also accepted
-	$mail->Port = 465;                                    // TCP port to connect to
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = "smtp.gmail.com";  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = "hash.coding.edge@gmail.com";                 // SMTP username
+    $mail->Password = "#trucoder";                           // SMTP password
+    $mail->SMTPSecure = "ssl";                         // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 465;                                    // TCP port to connect to
 
-	$mail->From = "hash.coding.edge@gmail.com";
-	$mail->FromName = "$name";
-	$mail->addAddress("hash.coding.edge@gmail.com", "Test User");     // Add a recipient
+    $mail->From = "hash.coding.edge@gmail.com";
+    $mail->FromName = "$name";
+    $mail->addAddress("hash.coding.edge@gmail.com", "Test User");     // Add a recipient
 
-	$mail->isHTML(true);                                  // Set email format to HTML
+    $mail->isHTML(true);                                  // Set email format to HTML
 
-	$mail->Subject = "Contact Us Form";
-	$mail->Body    = "Name: $name\n<br>Email: $email\n<br>Message: $message";
-	$mail->AltBody = "This is the body in plain text for non-HTML mail clients";
+    $mail->Subject = "Contact Us Form";
+    $mail->Body    = "Name: $name\n<br>Email: $email\n<br>Message: $message";
+    $mail->AltBody = "This is the body in plain text for non-HTML mail clients";
 
-	if(!$mail->send()) {
-		echo 'Message could not be sent.';
-		echo 'Mailer Error: ' . $mail->ErrorInfo;
-	} else {
-	    echo 'Message has been sent';
-	}
+    if(!$mail->send()) {
+        echo 'Message could not be sent.';
+        echo 'Mailer Error: ' . $mail->ErrorInfo;
+    } else {
+        echo 'Message has been sent';
+    }
 ?>
