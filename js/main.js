01eda1ce64cff981c4e8fed8ad979667e7dc6899
Jeremy Ko <jeremyko193@gmail.com>
2014-12-16 18:21:46 -0800

    AJAX-ify the Contact Us page, clean up form styles

--- js/main.js
+++ js/main.js
@@ -31,21 +31,25 @@
 
         clickHandle: function (e) {
             var item = $(e.currentTarget);
-            var template = 'template' + item.attr('data-template');
-            this.parentView.trigger('changeContent', template)
+            var template = item.attr('data-template');
+            this.parentView.trigger('changeContent', template);
         }
     });
 
     var Content = Backbone.View.extend({
+        events: {
+            'click #contact-us': 'send-email'
+        },
+
         initialize: function () {
             this.tmpl_cache = {};
-            var renderedHtml = this.fetchTemplate('template1', {});
+            var renderedHtml = this.fetchTemplate('1', {});
             this.$el.append(renderedHtml);
         },
 
         fetchTemplate: function (tmpl_name, tmpl_data) {
             if (!this.tmpl_cache[tmpl_name]) {
-                var tmpl_url = tmpl_name + '.html';
+                var tmpl_url = 'template' + tmpl_name + '.html';
 
                 var tmpl_string;
                 $.ajax({
@@ -68,8 +72,59 @@
             this.$el.append(renderedHtml);
             this.$el.slideDown({
                 duration: 400,
-                easing: "linear"
+                easing: 'linear'
             })
+            if (template === '3') {
+                $('#contact-us').on('click', {view: this}, this.validateContactForm);
+            }
+        },
+
+        validateContactForm: function (e) {
+            e.preventDefault();
+            var view = e.data.view;
+
+            var errorEl = $('.error');
+            errorEl.empty();
+
+            var hasError = false;
+            _.each(['message', 'email', 'name'], function (field) {
+                var fieldEl = view.$el.find('[name="' + field + '"]');
+                if (fieldEl.val && $.trim(fieldEl.val()) === '') {
+                    errorEl.empty();
+                    errorEl.append('The ' + field + ' field cannot be blank.');
+                    hasError = true;
+                    fieldEl.click();
+                }
+            });
+
+            if (!hasError) {
+                view.sendEmail(view);
+            }
+        },
+
+        sendEmail: function (view) {
+            var fields = view.$el.find('input, textarea');
+            var data = fields.serialize();
+            $.ajax({
+                url: 'php/ContactUs.php',
+                data: data,
+                method: 'POST',
+                success: function (data, status, xhr) {
+                    var feedbackEl = $('.feedback');
+                    feedbackEl.empty();
+                    feedbackEl.append('Your message has been sent.  Thank you for your feedback');
+                    for (var i = 0; i < fields.length; i++) {
+                        if ($(fields[i]).attr('type') !== 'submit') {
+                            $(fields[i]).val('');
+                        }
+                    }
+                },
+                error: function (xhr, status, error) {
+                    var errorEl = $('.error');
+                    errorEl.empty();
+                    errorEl.append('An unexpected error has occured.  Please try again later.');
+                }
+            });
         }
     });
     
--- php/ContactUs.php
+++ php/ContactUs.php
@@ -1,11 +1,11 @@
 <?php 
+    require "PHPMailerAutoload.php";
+    $mail = new PHPMailer();
+
     $name = $_POST["name"];
     $name = ($name ? $name : 'No name');
     $message = $_POST["message"];
     $email = $_POST["email"];
-
-    require "PHPMailerAutoload.php";
-    $mail = new PHPMailer();
 
     $mail->SMTPDebug = 3;
 
@@ -19,18 +19,18 @@
 
     $mail->From = "hash.coding.edge@gmail.com";
     $mail->FromName = "$name";
-    $mail->addAddress("hash.coding.edge@gmail.com", "Test User");     // Add a recipient
+    $mail->addAddress("hash.coding.edge@gmail.com", "codingEdge Team");     // Add a recipient
 
     $mail->isHTML(true);                                  // Set email format to HTML
 
     $mail->Subject = "Contact Us Form";
     $mail->Body    = "Name: $name\n<br>Email: $email\n<br>Message: $message";
-    $mail->AltBody = "This is the body in plain text for non-HTML mail clients";
 
     if(!$mail->send()) {
-        echo 'Message could not be sent.';
+        http_response_code(500);
         echo 'Mailer Error: ' . $mail->ErrorInfo;
     } else {
-        echo 'Message has been sent';
+        http_response_code(200);
+        echo 'success';
     }
 ?>
